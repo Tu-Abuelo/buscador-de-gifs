bf7ac8628ee435885516f568e74cfef20afba561
Tu-Abuelo <luisalbertovaldes1423@gmail.com>
2023-05-30 04:47:33 -0700

    asd8

--- src/Services/getGifs.js
+++ src/Services/getGifs.js
@@ -1,6 +1,6 @@
 const API_KEY = 'bpMXtHQFemqNnLncGQXHdi37WXZvE3vy'
 
-export default function getGifs ({ keyword = 'random', page = 0, rating = 'g', lang = 'en', limit = 2 }) {
+export default function getGifs ({ keyword = 'random', page = 0, rating = 'g', lang = 'en', limit = 12 }) {
   const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=${rating}&lang=${lang}`
 
   return fetch(apiURL)
